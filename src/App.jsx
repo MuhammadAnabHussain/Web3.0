c737e7a00b20ebe75cb3745d07fa1d143e5ac2a6
Muhammad Anab Hussain <ma564673@gmail.com>
2023-09-19 10:38:51 +0500

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import {Welcome,Loader,Services,Footer,Transaction,Navbar} from "./components";
+const  App = () => {
+
+  return (
+   <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+        <Navbar/>
+        <Welcome/>
+    </div>
+      <Services/>
+      <Transaction/>
+      <Footer/>
+   </div>
+  )
+}
+
+export default App
