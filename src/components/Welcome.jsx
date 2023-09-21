7eba63375baefa436b5d383e90a238c2da1c3372
Muhammad Anab Hussain <ma564673@gmail.com>
2023-09-21 09:04:45 +0500

    adding inputs

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,10 +3,23 @@ import { SiEthereum } from 'react-icons/si';
 import { BsInfoCircle } from 'react-icons/bs';
 import { Loader } from './';
 const commonStyles='min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white';
+
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+    <input
+      placeholder={placeholder}
+      type={type}
+      step="0.0001"
+      value={value}
+      onChange={(e) => handleChange(e, name)}
+      className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+    />
+  );
+  
 const Welcome = ()=>{
     const connectWallet=()=>{
        
     }
+
 
     return (
        <div className="flex w-full justify-center items-center">
@@ -60,8 +73,16 @@ const Welcome = ()=>{
                             </p>
                         </div>
                     </div>
+                </div>
+                
+                <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+                <Input placeholder="Address To" name="addressTo" type="text" handleChange={()=>{}} />
+            <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={()=>{}} />
+            <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={()=>{}} />
+            <Input placeholder="Enter Message" name="message" type="text" handleChange={()=>{}} />
 
                 </div>
+
             </div>
         </div>
        </div>
